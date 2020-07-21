d2e27162b974a74d4ccd0807ec9960688489411e
chuyong <chuyong@bilibili.com>
2020-07-21 18:48:29 +0800

    Post processing

---
+++ HexMap/Assets/Scripts/Postprocess/EdgeDetectionSobel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(EdgeDetectionSobelRenderer), PostProcessEvent.AfterStack, "Custom/EdgeDetectionSobel")]
+public sealed class EdgeDetectionSobel : PostProcessEffectSettings
+{
+    [Range(0.05f, 5.0f)]
+    public FloatParameter edgeWidth = new FloatParameter { value = 0.3f };
+
+    [ColorUsage(true, true)]
+    public ColorParameter edgeColor = new ColorParameter { value = new Color(0.0f, 0.0f, 0.0f, 1) };
+}
+
+public sealed class EdgeDetectionSobelRenderer : PostProcessEffectRenderer<EdgeDetectionSobel>
+{
+    private Shader shader;
+    public override void Init()
+    {
+        shader = Shader.Find("Hidden/Custom/EdgeDetectionSobel");
+    }
+
+    static class ShaderIDs
+    {
+        internal static readonly int Params = Shader.PropertyToID("_Params");
+        internal static readonly int EdgeColor = Shader.PropertyToID("_EdgeColor");
+    }
+
+    public override void Render(PostProcessRenderContext context)
+    {
+        PropertySheet sheet = context.propertySheets.Get(shader);
+        sheet.properties.SetVector(ShaderIDs.Params, new Vector2(settings.edgeWidth, 0));
+        sheet.properties.SetColor(ShaderIDs.EdgeColor, settings.edgeColor);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ HexMap/Assets/Scripts/Postprocess/Grayscale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(GrayscaleRenderer), PostProcessEvent.AfterStack, "Custom/Grayscale")]
+public sealed class Grayscale : PostProcessEffectSettings
+{
+    [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
+    public FloatParameter blend = new FloatParameter { value = 0.5f };
+}
+
+public sealed class GrayscaleRenderer : PostProcessEffectRenderer<Grayscale>
+{
+    private Shader shader;
+    static readonly int BlendProper = Shader.PropertyToID("_Blend");
+    public override void Init()
+    {
+        shader = Shader.Find("Hidden/Custom/ImageGrayscale");
+    }
+
+    public override void Render(PostProcessRenderContext context)
+    {
+        PropertySheet sheet = context.propertySheets.Get(shader);
+        sheet.properties.SetFloat(BlendProper, settings.blend);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
